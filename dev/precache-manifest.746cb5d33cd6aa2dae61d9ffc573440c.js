71c169307a4e1129e55c9a5ae52f9302bb0f6a4e
Steven Etgen <setgen5@gmail.com>
2020-04-17 22:36:29 -0400

    Updates

---
+++ dev/precache-manifest.746cb5d33cd6aa2dae61d9ffc573440c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b158120c22b5297f9cb9413b7cfddd",
+    "url": "/shark-evolution/devassets/.DS_Store"
+  },
+  {
+    "revision": "deec73c79c02e771f5fe18be9c63cf89",
+    "url": "/shark-evolution/devassets/citations.md"
+  },
+  {
+    "revision": "9d79a2ec01455afa082d23e42f457801",
+    "url": "/shark-evolution/devassets/font/font.png"
+  },
+  {
+    "revision": "38c02ad8ccaf9579150bb10d283e2b0c",
+    "url": "/shark-evolution/devassets/font/font.xml"
+  },
+  {
+    "revision": "76f6fac212dc7b831d40a80408047a80",
+    "url": "/shark-evolution/devassets/icons/.DS_Store"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/shark-evolution/devassets/icons/favicon.ico"
+  },
+  {
+    "revision": "8ab30def0ad109a97117bd62db942d75",
+    "url": "/shark-evolution/devassets/icons/icons-16.png"
+  },
+  {
+    "revision": "feee71db2bb3ba5d11b26658baf97783",
+    "url": "/shark-evolution/devassets/icons/icons-192.png"
+  },
+  {
+    "revision": "cb71713be71d0b8820c7ea6af9c5ddc6",
+    "url": "/shark-evolution/devassets/icons/icons-512.png"
+  },
+  {
+    "revision": "200ce541edfaa71be93a1041736d1fee",
+    "url": "/shark-evolution/devassets/icons/icons8-game-controller-16.png"
+  },
+  {
+    "revision": "54005b97283fd24e68930f75d3a7e153",
+    "url": "/shark-evolution/devassets/images/.DS_Store"
+  },
+  {
+    "revision": "1cdca402420712b4f35917cad50e43a3",
+    "url": "/shark-evolution/devassets/images/background.png"
+  },
+  {
+    "revision": "bd6819cc933aac78f9c7ff50c67cfe6d",
+    "url": "/shark-evolution/devassets/images/play1.png"
+  },
+  {
+    "revision": "25bd810f1df36c3dd004c47330bcc820",
+    "url": "/shark-evolution/devassets/images/play2.png"
+  },
+  {
+    "revision": "1cdca402420712b4f35917cad50e43a3",
+    "url": "/shark-evolution/devassets/images/splash.png"
+  },
+  {
+    "revision": "fb198af7a77f4338ab91b57493ddcb84",
+    "url": "/shark-evolution/devassets/sounds/.DS_Store"
+  },
+  {
+    "revision": "2101f87289e14406eb4b215cf55a3ebc",
+    "url": "/shark-evolution/devassets/sounds/pickup.mp3"
+  },
+  {
+    "revision": "2caf90e1d1576c0a7891bfb9723ded61",
+    "url": "/shark-evolution/devassets/sounds/pickup.ogg"
+  },
+  {
+    "revision": "5eb32f9131945d2aa6627642c97001d7",
+    "url": "/shark-evolution/devassets/spritesheets/.DS_Store"
+  },
+  {
+    "revision": "f110c761d8de12427ae024a9f3091c68",
+    "url": "/shark-evolution/devassets/spritesheets/fish1.png"
+  },
+  {
+    "revision": "81f5395a1312b255fda148d3a47536fb",
+    "url": "/shark-evolution/devassets/spritesheets/fish2.png"
+  },
+  {
+    "revision": "968810487b0e56bf5fae3571cc24db08",
+    "url": "/shark-evolution/devassets/spritesheets/fish_spritesheet_1.png"
+  },
+  {
+    "revision": "82b75877a6feb963b3c8a294f6125920",
+    "url": "/shark-evolution/devassets/spritesheets/fish_spritesheet_2.png"
+  },
+  {
+    "revision": "5e1b308cb06835d1b8c6175f4ee48293",
+    "url": "/shark-evolution/devassets/spritesheets/fish_spritesheet_3.png"
+  },
+  {
+    "revision": "0106c6a27adef66d4917f2256ffbc0a7",
+    "url": "/shark-evolution/devassets/spritesheets/fish_spritesheet_4.png"
+  },
+  {
+    "revision": "fb24d5b5ce3fac71b85fdfa12c51bde8",
+    "url": "/shark-evolution/devassets/spritesheets/fish_spritesheet_5.png"
+  },
+  {
+    "revision": "5180cb0e47e6d5e54ba19a5c2ee26c8b",
+    "url": "/shark-evolution/devassets/spritesheets/megalodon.png"
+  },
+  {
+    "revision": "ad4570fa67825ee18765a31762ee2263",
+    "url": "/shark-evolution/devassets/spritesheets/shark_sprites.png"
+  },
+  {
+    "revision": "1c8fc1c25f5fe59b22ea335c2124e62d",
+    "url": "/shark-evolution/devassets/text/inputform.html"
+  },
+  {
+    "revision": "53cefca2a0f75654486b180949686165",
+    "url": "/shark-evolution/devfavicon.ico"
+  },
+  {
+    "revision": "f85cbbb6587ac3309f61",
+    "url": "/shark-evolution/devgen.main.dba834666c3a6109c412.bundle.js"
+  },
+  {
+    "revision": "9ecf88fe9811c5d77969",
+    "url": "/shark-evolution/devgen.vendors.be91aecff310ec1b870c.bundle.js"
+  },
+  {
+    "revision": "447c562dda133bca211befc70edfdf11",
+    "url": "/shark-evolution/devindex.html"
+  },
+  {
+    "revision": "4e5302c74223c4a425346430e90c63d7",
+    "url": "/shark-evolution/devmanifest.json"
+  },
+  {
+    "revision": "701445d8817f73e3da17e450b69faa36",
+    "url": "/shark-evolution/devsw.js"
+  }
+]);
---
+++ dev/sw.js
@@ -0,0 +1,11 @@
+importScripts("/shark-evolution/devprecache-manifest.746cb5d33cd6aa2dae61d9ffc573440c.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/**
+ * You should only modify this, if you know what you are doing.
+ * This phaser template is using workbox (https://developers.google.com/web/tools/workbox/)
+ * to precache all assets.
+ * It uses the InjectManifest function from 'workbox-webpack-plugin' inside
+ * webpack/webpack.common.js
+ */
+workbox.precaching.precacheAndRoute(__precacheManifest)
+
