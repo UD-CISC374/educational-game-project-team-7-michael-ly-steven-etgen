db72d2064a179a1ab92e44873aaf43ab332c686f
Steven Etgen <setgen5@gmail.com>
2020-05-18 22:36:30 -0400

    Updates

--- dev/sw.js
+++ dev/sw.js
@@ -1,4 +1,4 @@
-importScripts("/educational-game-project-team-7-michael-ly-steven-etgen/devprecache-manifest.6407c1fa74505f032f7234185b23f85f.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("/educational-game-project-team-7-michael-ly-steven-etgen/devprecache-manifest.c949e127ff7ccff12c2e3c864b6a06ce.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 /**
  * You should only modify this, if you know what you are doing.
